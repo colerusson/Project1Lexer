f3e22cb6aef4f25831edd8a876823a150aa16bd8
coler <colerusson2@gmail.com>
2022-07-09 12:58:54 -0600

    new commit

--- main.cpp
+++ main.cpp
@@ -9,67 +9,67 @@
 
 using namespace std;
 
-//int main(int argc, char* argv[]) {
-//
-//    ifstream inputFile(argv[1]);
-//    string stringFile;
-//    stringFile.assign((istreambuf_iterator<char>(inputFile)), (istreambuf_iterator<char>()));
-//
-//    try {
-//        vector<Token> tokens;
-//        Lexer lexer;
-//        tokens = lexer.run(stringFile);
-//        Parser parser = Parser(tokens);
-//        parser.run();
-//        cout << "Success!";
-//    }
-//    catch(Token errorToken) {
-//        cout << "Failure!" << endl << " " << errorToken.toString();
-//    }
-//
-//    return 0;
-//}
+int main(int argc, char* argv[]) {
 
-int main() {
-    DatalogProgram program;
+    ifstream inputFile(argv[1]);
+    string stringFile;
+    stringFile.assign((istreambuf_iterator<char>(inputFile)), (istreambuf_iterator<char>()));
 
-    Predicate snapScheme;
-    snapScheme.setName("snap");
-    snapScheme.addParameter("S");
-    snapScheme.addParameter("N");
-    snapScheme.addParameter("A");
-    snapScheme.addParameter("P");
-    program.addScheme(snapScheme);
-
-    Predicate addressScheme;
-    addressScheme.setName("HasSameAddress");
-    addressScheme.addParameter("X");
-    addressScheme.addParameter("Y");
-    program.addScheme(addressScheme);
-
-    Predicate snapFact;
-    snapFact.setName("snap");
-    snapFact.addParameter("\'12345\'");
-    snapFact.addParameter("\'C.Brown\'");
-    snapFact.addParameter("\'12 Apple\'");
-    snapFact.addParameter("\'555-1234\'");
-    program.addFact(snapFact);
-
-    Rule rulePredicate;
-    rulePredicate.setHead(addressScheme);
-    rulePredicate.addPredicate(snapScheme);
-    rulePredicate.addPredicate(snapScheme);
-    program.addRule(rulePredicate);
-
-    Predicate addressQuery;
-    addressQuery.setName("HasSameAddress");
-    addressQuery.addParameter("\'Snoopy\'");
-    addressQuery.addParameter("Who");
-    program.addQuery(addressQuery);
-
-    program.addDomain("\'12 Apple\'");
-
-    cout << program.toString();
+    try {
+        vector<Token> tokens;
+        Lexer lexer;
+        tokens = lexer.run(stringFile);
+        Parser parser = Parser(tokens);
+        parser.run();
+        cout << "Success!";
+    }
+    catch(Token errorToken) {
+        cout << "Failure!" << endl << " " << errorToken.toString();
+    }
 
     return 0;
 }
+
+//int main() {
+//    DatalogProgram program;
+//
+//    Predicate snapScheme;
+//    snapScheme.setName("snap");
+//    snapScheme.addParameter("S");
+//    snapScheme.addParameter("N");
+//    snapScheme.addParameter("A");
+//    snapScheme.addParameter("P");
+//    program.addScheme(snapScheme);
+//
+//    Predicate addressScheme;
+//    addressScheme.setName("HasSameAddress");
+//    addressScheme.addParameter("X");
+//    addressScheme.addParameter("Y");
+//    program.addScheme(addressScheme);
+//
+//    Predicate snapFact;
+//    snapFact.setName("snap");
+//    snapFact.addParameter("\'12345\'");
+//    snapFact.addParameter("\'C.Brown\'");
+//    snapFact.addParameter("\'12 Apple\'");
+//    snapFact.addParameter("\'555-1234\'");
+//    program.addFact(snapFact);
+//
+//    Rule rulePredicate;
+//    rulePredicate.setHead(addressScheme);
+//    rulePredicate.addPredicate(snapScheme);
+//    rulePredicate.addPredicate(snapScheme);
+//    program.addRule(rulePredicate);
+//
+//    Predicate addressQuery;
+//    addressQuery.setName("HasSameAddress");
+//    addressQuery.addParameter("\'Snoopy\'");
+//    addressQuery.addParameter("Who");
+//    program.addQuery(addressQuery);
+//
+//    program.addDomain("\'12 Apple\'");
+//
+//    cout << program.toString();
+//
+//    return 0;
+//}
